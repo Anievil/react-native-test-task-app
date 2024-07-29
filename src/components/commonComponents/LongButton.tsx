d71e27fd8e4ff9d2e695929ef4ceabd9b71db3a6
Oleksii Bazylevskyi <oleksiibazylevskyi@Oleksiis-iMac-Pro.local>
2024-07-29 15:32:20 +0300

    fix: fix long button margin. Test ios

--- src/components/commonComponents/LongButton.tsx
+++ src/components/commonComponents/LongButton.tsx
@@ -8,7 +8,7 @@ const LongButton: FC<LongButtonProps> = ({ onPress, text }) => {
   return (
     <TouchableOpacity
       onPress={onPress}
-      tw="mx-[20px] bg-black py-[20px] rounded-full"
+      tw="mx-[20px] bg-black py-[20px] mb-[5px] rounded-full"
     >
       <StyledText tw="text-white text-normal leading-[18.5px] text-center">
         {text}
